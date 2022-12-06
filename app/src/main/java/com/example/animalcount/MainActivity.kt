bcc4bd9f11175c7533f2531a178764efb08bd5e0
Taylor Sandusky <taylorsand11@comcast.net>
2022-12-06 10:46:36 -0800

    Improve list UI
    fix

--- app/src/main/java/com/example/animalcount/MainActivity.kt
+++ app/src/main/java/com/example/animalcount/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.animalcount
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -17,6 +21,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.animalcount.model.Animal
@@ -27,14 +34,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AnimalCountTheme {
-                AnimalList()
+                AnimalListPage()
             }
         }
     }
 }
 
 @Composable
-fun AnimalList() {
+fun AnimalListPage(
+    modifier: Modifier = Modifier
+) {
     val animalList = listOf(
         Animal(name = "Cat"),
         Animal(name = "Cow"),
@@ -44,7 +53,10 @@ fun AnimalList() {
         Animal(name = "Sea Lion"),
     )
 
-    Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = modifier,
+    ) {
         for (animal in animalList) {
             AnimalRow(animal)
         }
@@ -52,22 +64,40 @@ fun AnimalList() {
 }
 
 @Composable
-fun AnimalRow(animal: Animal) {
+fun AnimalRow(
+    animal: Animal,
+    modifier: Modifier = Modifier,
+) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement
+            .spacedBy(
+                space = 10.dp,
+                alignment = Alignment.CenterHorizontally
+            ),
+        modifier = modifier.fillMaxWidth(),
     ) {
         var count by remember { mutableStateOf(animal.count) }
         IconButton(
-            onClick = { count-- }
+            onClick = { count-- },
+            modifier = Modifier
+                .clip(CircleShape)
+                .background(Color.Red),
         ) {
             Icon(
                 painter = painterResource(R.drawable.baseline_remove_24),
                 contentDescription = "Remove one"
             )
         }
-        Text("${animal.name}: $count")
+        Text(
+            text = "${animal.name}: $count",
+            style = MaterialTheme.typography.titleLarge,
+        )
         IconButton(
-            onClick = { count++ }
+            onClick = { count++ },
+            modifier = modifier // TODO: fix ripple
+                .clip(CircleShape)
+                .background(Color.Green),
         ) {
             Icon(
                 Icons.Default.Add,
