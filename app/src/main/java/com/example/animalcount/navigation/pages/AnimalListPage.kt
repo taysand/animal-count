6350ce77092fd3d6922cab5285bcab1301620297
Taylor Sandusky <taylorsand11@comcast.net>
2024-07-05 10:11:37 -0700

    Move theme out of scaffold, add previews, add font and padding tokens, create test animal list, separate increase and decrease buttons

--- app/src/main/java/com/example/animalcount/navigation/pages/AnimalListPage.kt
+++ app/src/main/java/com/example/animalcount/navigation/pages/AnimalListPage.kt
@@ -4,29 +4,29 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.animalcount.model.Animal
 import com.example.animalcount.ui.components.AnimalRow
+import com.example.animalcount.ui.theme.LargePadding
+import com.example.animalcount.util.SampleAnimalList
 
 @Composable
 fun AnimalListPage(
+    list: List<Animal>,
     modifier: Modifier = Modifier
 ) {
-    val animalList = listOf(
-        Animal(name = "Cat"),
-        Animal(name = "Cow"),
-        Animal(name = "Crab"),
-        Animal(name = "Dog"),
-        Animal(name = "Seal"),
-        Animal(name = "Sea Lion"),
-    )
-
     Column(
-        verticalArrangement = Arrangement.spacedBy(20.dp),
+        verticalArrangement = Arrangement.spacedBy(LargePadding),
         modifier = modifier,
     ) {
-        for (animal in animalList) {
+        for (animal in list) {
             AnimalRow(animal)
         }
     }
+}
+
+@Preview
+@Composable
+fun AnimalListPagePreview() {
+    AnimalListPage(SampleAnimalList)
 }
