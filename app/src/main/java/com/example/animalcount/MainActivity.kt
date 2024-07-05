b7914d57c91e2972999b29686e62f417f1747ca4
Taylor Sandusky <taylorsand11@comcast.net>
2024-07-05 10:43:31 -0700

    Hilt setup

--- app/src/main/java/com/example/animalcount/MainActivity.kt
+++ app/src/main/java/com/example/animalcount/MainActivity.kt
@@ -7,7 +7,9 @@ import com.example.animalcount.navigation.pages.AnimalListPage
 import com.example.animalcount.ui.components.AnimalCountScaffold
 import com.example.animalcount.ui.theme.AnimalCountTheme
 import com.example.animalcount.util.SampleAnimalList
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
