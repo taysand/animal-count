7901c9e365b4c6bc14e1355b3212fe60e0cd6e3c
Taylor Sandusky <taylorsand11@comcast.net>
2024-08-06 16:59:12 -0700

    Support adding animals with custom name

--- app/src/main/java/com/example/animalcount/ui/pages/add/AddAnimalPage.kt
+++ app/src/main/java/com/example/animalcount/ui/pages/add/AddAnimalPage.kt
@@ -3,13 +3,16 @@ package com.example.animalcount.ui.pages.add
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.animalcount.R
-import com.example.animalcount.ui.pages.home.HomePageViewModel
 
 @Composable
 fun AddAnimalPage(
@@ -17,13 +20,29 @@ fun AddAnimalPage(
     viewModel: AddAnimalPageViewModel = hiltViewModel(),
     navigateBack: () -> Unit,
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Column(
         modifier = modifier,
     ) {
+        TextField(
+            value = uiState.name,
+            singleLine = true,
+            onValueChange = { name ->
+                viewModel.updateAnimalName(name)
+            },
+            placeholder = {
+                Text(stringResource(id = R.string.animal_name))
+            },
+        )
         Button(
             onClick = {
-                viewModel.addAnimal("new animal")
-                navigateBack()
+                if (uiState.name.isNotEmpty()) {
+                    keyboardController?.hide()
+                    viewModel.addAnimal()
+                    navigateBack()
+                }
             },
         ) {
             Text(stringResource(id = R.string.add_animal))
